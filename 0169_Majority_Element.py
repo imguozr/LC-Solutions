f390fe4436b2ae2c3104b1b8e4cabf1e455c5096
imguozr <imguozr@gmail.com>
2020-02-25 23:24:06 -0600

    2020/02/25

---
+++ 0169_Majority_Element.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+        Boyer-Moore Majority Vote Algorithm. Like # 229.
+    """
+
+    def majorityElement(self, nums: list[int]) -> int:
+        res, cnt = 0, 0
+        for num in nums:
+            if not cnt:
+                res, cnt = num, 1
+            elif num == res:
+                cnt += 1
+            else:
+                cnt -= 1
+        return res
---
+++ 0229_Majority_Element_II.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+        We can use a Counter to solve such problem if not having limitations on O(1) space.
+        Instead, we choose Boyer-Moore Majority Vote Algorithm.
+        https://gregable.com/2013/10/majority-vote-algorithm-find-majority.html
+
+        And because we have most 2 variables, we can use 2 counts and 2 candidates.
+    """
+
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        if not nums:
+            return []
+        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        return [n for n in (candidate1, candidate2)
+                if nums.count(n) > len(nums) // 3]
